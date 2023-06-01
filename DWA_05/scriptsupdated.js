bddcd4908c77b49f4af039b9ab75a8b40319bc92
Zamavundla <zsdouzee8@gmail.com>
2023-06-01 10:59:36 +0200

    final updates

--- DWA_05/scriptsupdated.js
+++ DWA_05/scriptsupdated.js
@@ -2,36 +2,27 @@
 
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+const body = document.querySelector("body")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  
+  if (dividend === "" || divider === "") {
+		result.innerText = "Division not performed. Both values are required in inputs. Try again"
 
-  const dividendInput = document.getElementById("dividend");
-  const dividerInput = document.getElementById("divider");
-  
-  //const dividend = parseFloat(dividendInput.value);
-  //const divider = parseFloat(dividerInput.value);
-  
-  if (isNaN(dividend) || isNaN(divider)) {
-    result.innerText = "Something critical went wrong. Please reload the page.";
-    console.error("Invalid input. Please provide valid numbers.");
-    return;
-  }
+	} else if (dividend < 0 || divider < 0) {
+		result.innerText = "Division not performed. Invalid number provided. Try again"
+		console.error("Division not performed. Invalid number provided. Try again");
 
-  if (divider === 0) {
-    result.innerText = "Division not performed. Cannot divide by zero. Try again.";
-    console.error("Division by zero. Please provide a non-zero divider.");
-    return;
-  }
-  
-  if (Number.isInteger(dividend / divider)) {
-    result.innerText = dividend / divider;
-  } else {
-    result.innerText = Math.floor(dividend / divider);
-    console.warn("Division resulted in a decimal number. Displaying the integer part only.");
-  }
+	} else if (dividend == "YOLO" || divider == "+++") {
+		body.innerText = "Something critical went wrong. Please reload the page"
+		console.error("Something critical went wrong. Please reload the page");
+	} else {
+		result.innerText = Math.floor(dividend / divider);
+	}
+
 });
