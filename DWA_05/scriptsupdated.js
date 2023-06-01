d4119d328afad2afe5a746e74f26c89a1ff6944a
Zamavundla <zsdouzee8@gmail.com>
2023-06-01 10:31:41 +0200

    new scripts added

---
+++ DWA_05/scriptsupdated.js
@@ -0,0 +1,32 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const dividendInput = document.getElementById("dividend");
+  const dividerInput = document.getElementById("divider");
+  
+  const dividend = parseFloat(dividendInput.value);
+  const divider = parseFloat(dividerInput.value);
+  
+  if (isNaN(dividend) || isNaN(divider)) {
+    result.innerText = "Something critical went wrong. Please reload the page.";
+    console.error("Invalid input. Please provide valid numbers.");
+    return;
+  }
+
+  if (divider === 0) {
+    result.innerText = "Division not performed. Cannot divide by zero. Try again.";
+    console.error("Division by zero. Please provide a non-zero divider.");
+    return;
+  }
+  
+  if (Number.isInteger(dividend / divider)) {
+    result.innerText = dividend / divider;
+  } else {
+    result.innerText = Math.floor(dividend / divider);
+    console.warn("Division resulted in a decimal number. Displaying the integer part only.");
+  }
+});
