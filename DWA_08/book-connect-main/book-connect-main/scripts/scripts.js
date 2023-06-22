a6d2a347479aff27d66f1113f9256e40464f8ef2
Zamavundla <zsdouzee8@gmail.com>
2023-06-22 13:23:39 +0200

    Final Updates

--- DWA_08/book-connect-main/book-connect-main/scripts/scripts.js
+++ DWA_08/book-connect-main/book-connect-main/scripts/scripts.js
@@ -1,10 +1,42 @@
-//@ts-check
+// @ts-nocheck
 
 import { BOOKS_PER_PAGE, authors, genres, books } from "./data.js";
 
-//These are query selectors for each user story which makes the bookconnect works.
+/**
+ * @typedef {object} HTMLSelectors
+ * @prop {HTMLButtonElement} button - The HTML button element.
+ * @prop {HTMLDivElement} dialog - The HTML div element representing a dialog.
+ * @prop {HTMLFormElement} form - The HTML form element.
+ * @prop {HTMLSelectElement} theme - The HTML select element for theme selection.
+ * @prop {HTMLButtonElement} cancel - The HTML button element for cancel action.
+ * @prop {HTMLButtonElement} save - The HTML button element for save action.
+ */
+
+/**
+ * @typedef {object} ListOverlayElements
+ * @prop {HTMLElement} active - The active overlay element.
+ * @prop {HTMLImageElement} blur - The HTML image element representing blur effect.
+ * @prop {HTMLHeadingElement} title - The HTML heading element for book title.
+ * @prop {HTMLParagraphElement} subtitle - The HTML paragraph element for book subtitle.
+ * @prop {HTMLDivElement} description - The HTML div element for book description.
+ * @prop {HTMLButtonElement} close - The HTML button element for closing the overlay.
+ * @prop {NodeListOf<HTMLImageElement>} image - The HTML image elements representing book images.
+ */
+
+/**
+ * @typedef {object} HTMLSections
+ * @prop {HTMLSelectors} settings - The settings section HTML selectors.
+ * @prop {HTMLSelectors} search - The search section HTML selectors.
+ * @prop {object} list - The list section HTML selectors.
+ * @prop {HTMLUListElement} list.items - The HTML ul element for list items.
+ * @prop {HTMLDivElement} list.message - The HTML div element for list message.
+ * @prop {HTMLButtonElement} list.button - The HTML button element for show more action.
+ * @prop {NodeListOf<HTMLButtonElement>} list.preview - The HTML button elements for book preview.
+ * @prop {ListOverlayElements} list.overlay - The elements of book preview overlay.
+ */
+
+/** @type {HTMLSections} */ `html`
 const html = {
-  
   settings: {
     button: document.querySelector('[data-header-settings]'),
     dialog: document.querySelector('[data-settings-overlay]'),
@@ -13,18 +45,16 @@ const html = {
     cancel: document.querySelector('[data-settings-cancel]'),
     save: document.querySelector('[data-settings-save]')
   },
- 
   search: {
-      button: document.querySelector('[data-header-search]'),
-      dialog: document.querySelector('[data-search-overlay]'),
-      cancel: document.querySelector('[data-search-cancel]'),
-      form: document.querySelector('[data-search-form]'),
-      title: document.querySelector('[data-search-title]'),
-      genre: document.querySelector('[data-search-genres]'),
-      author: document.querySelector('[data-search-authors]'),
-      submit: document.querySelector('[data-search-submit]')
+    button: document.querySelector('[data-header-search]'),
+    dialog: document.querySelector('[data-search-overlay]'),
+    cancel: document.querySelector('[data-search-cancel]'),
+    form: document.querySelector('[data-search-form]'),
+    title: document.querySelector('[data-search-title]'),
+    genre: document.querySelector('[data-search-genres]'),
+    author: document.querySelector('[data-search-authors]'),
+    submit: document.querySelector('[data-search-submit]')
   },
-  
   list: {
     items: document.querySelector('[data-list-items]'),
     message: document.querySelector('[data-list-message]'),
@@ -38,31 +68,32 @@ const html = {
       description: document.querySelector('[data-list-description]'),
       close: document.querySelector('[data-list-close]'),
       image: document.querySelectorAll('[data-list-image]')
-
+    }
   }
-  },
-}
+};
 
-
-
-// Variables with global scope to use across functions
+/** @type {DocumentFragment} */
 const fragment = document.createDocumentFragment();
+
+/** @type {HTMLElement} */
 const area = document.querySelector('[data-list-items]');
+
+/** @type {number} */
 let index = 0;
 
 /**
  * Loads the next set of books when the "Show More" button is clicked.
  * Displays BOOKS_PER_PAGE number of books each time.
  * Uses the global index variable to track the number of books loaded so far.
+ * 
+ * @param {Event} event - The click event object.
  */
 const loadBooks = (event) => {
   event.preventDefault();
-  // @ts-ignore
   html.list.message.classList = 'list__message';
 
   const extracted = books.slice(index, index + BOOKS_PER_PAGE);
   const booksLeft = books.length - index;
-  // @ts-ignore
   html.list.button.textContent = `Show More (${booksLeft})`;
 
   for (let i = index; i < index + BOOKS_PER_PAGE; i++) {
@@ -70,10 +101,9 @@ const loadBooks = (event) => {
     const { image, title, author: authorId, id } = book;
 
     const element = document.createElement('button');
-    // @ts-ignore
     element.classList = 'preview';
     element.setAttribute('id', id);
-    element.innerHTML = /* html */ `
+    element.innerHTML = `
       <img class="preview__image" src="${image}"/>
       <div class="preview__info" data-box>
         <h3 class="preview__title">${title}</h3>
@@ -83,108 +113,92 @@ const loadBooks = (event) => {
     fragment.appendChild(element);
   }
 
-  // @ts-ignore
   area.appendChild(fragment);
   index += extracted.length;
 };
 
-// @ts-ignore
 html.list.button.addEventListener('click', loadBooks);
 window.addEventListener('load', loadBooks);
 
 /**
  * Event listener that displays the book preview when a book is clicked.
+ * 
+ * @param {MouseEvent} event - The click event object.
  */
 document.addEventListener('click', (event) => {
-  // @ts-ignore
   if (html.list.overlay.active.hasAttribute('open')) {
-    // @ts-ignore
     html.list.overlay.active.removeAttribute('open');
   } else {
-    // @ts-ignore
     const button = event.target.closest('.preview');
     if (button == null) {
       return;
     }
 
     const book = books.find((book) => book.id === button.id);
-    // @ts-ignore
     const year = new Date(book.published).getFullYear();
 
-    // @ts-ignore
     const { title, image, description, author: authorId } = book;
     const titleElement = html.list.overlay.title;
-    // @ts-ignore
     titleElement.innerText = title;
 
     const imageElement = document.querySelector('[data-list-image]');
-    // @ts-ignore
     imageElement.src = image;
 
     const blurElement = document.querySelector('[data-list-blur]');
-    // @ts-ignore
     blurElement.src = image;
 
     const descriptionElement = html.list.overlay.description;
-    // @ts-ignore
     descriptionElement.innerText = description;
 
     const subtitleElement = html.list.overlay.subtitle;
-    // @ts-ignore
     subtitleElement.innerText = `${authors[authorId]} (${year})`;
 
-    // @ts-ignore
     html.list.overlay.active.setAttribute('open', true);
   }
 });
 
 /**
  * Opens and closes the search overlay.
+ * 
+ * @param {MouseEvent} event - The click event object.
  */
 const handleSearchToggle = (event) => {
   event.preventDefault();
-  // @ts-ignore
   if (html.search.dialog.hasAttribute('open')) {
-    // @ts-ignore
     html.search.dialog.removeAttribute('open');
   } else {
-    // @ts-ignore
     html.search.dialog.setAttribute('open', true);
   }
 };
 
-// @ts-ignore
 html.search.button.addEventListener('click', handleSearchToggle);
-// @ts-ignore
 html.search.cancel.addEventListener('click', handleSearchToggle);
 
 /**
  * Opens and closes the settings overlay.
+ * 
+ * @param {MouseEvent} event - The click event object.
  */
 const handleSettingsToggle = (event) => {
   event.preventDefault();
-  // @ts-ignore
   if (html.settings.dialog.hasAttribute('open')) {
-    // @ts-ignore
     html.settings.dialog.removeAttribute('open');
   } else {
-    // @ts-ignore
     html.settings.dialog.setAttribute('open', true);
   }
 };
 
-// @ts-ignore
 html.settings.button.addEventListener('click', handleSettingsToggle);
-// @ts-ignore
 html.settings.cancel.addEventListener('click', handleSettingsToggle);
 
 /**
  * Saves the selected theme and changes colors to light or dark
  * depending on the saved selection.
+ * 
+ * @param {MouseEvent} event - The click event object.
  */
 const handleSettingsSave = (event) => {
   event.preventDefault();
-  // @ts-ignore
   if (html.settings.theme.value === 'night') {
     document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
     document.documentElement.style.setProperty('--color-light', '10, 10, 20');
@@ -192,15 +206,15 @@ const handleSettingsSave = (event) => {
     document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
     document.documentElement.style.setProperty('--color-light', '255, 255, 255');
   }
-  // @ts-ignore
   html.settings.dialog.removeAttribute('open');
 };
 
-// @ts-ignore
 html.settings.save.addEventListener('click', handleSettingsSave);
 
 /**
  * Adds genre options to the select element on the search overlay.
+ * 
+ * @param {MouseEvent} event - The click event object.
  */
 const createGenreOptionsHtml = (event) => {
   event.preventDefault();
@@ -213,15 +227,15 @@ const createGenreOptionsHtml = (event) => {
     fragment.appendChild(option);
   }
 
-  // @ts-ignore
   html.search.genre.appendChild(fragment);
 };
 
-// @ts-ignore
 html.search.button.addEventListener('click', createGenreOptionsHtml);
 
 /**
  * Adds author options to the select element on the search overlay.
+ * 
+ * @param {MouseEvent} event - The click event object.
  */
 const createAuthorOptionsHtml = (event) => {
   event.preventDefault();
@@ -234,24 +248,21 @@ const createAuthorOptionsHtml = (event) => {
     fragment.appendChild(option);
   }
 
-  // @ts-ignore
   html.search.author.appendChild(fragment);
 };
 
-// @ts-ignore
 html.search.author.addEventListener('click', createAuthorOptionsHtml);
 
 /**
  * Searches for books based on the selected search criteria.
+ * 
+ * @param {MouseEvent} event - The click event object.
  */
 const handleSearchSubmit = (event) => {
   event.preventDefault();
   const search = {
-    // @ts-ignore
     title: html.search.title.value,
-    // @ts-ignore
     author: html.search.author.value,
-    // @ts-ignore
     genre: html.search.genre.value,
   };
 
@@ -280,62 +291,47 @@ const handleSearchSubmit = (event) => {
     }
   }
 
-  // @ts-ignore
   html.search.genre.value = 'All genres';
-  // @ts-ignore
   html.search.author.value = 'All authors';
-  // @ts-ignore
   html.search.title.value = '';
 
   return handleSearchResults(found[0]);
 };
 
-// @ts-ignore
 html.search.submit.addEventListener('click', handleSearchSubmit);
 
 /**
  * Displays the search results in the main body.
  * Shows an error message if no results are found.
+ * 
+ * @param {Array} found - Array of found books.
  */
 const handleSearchResults = (found) => {
   if (typeof found === 'undefined') {
-    // @ts-ignore
     html.search.dialog.removeAttribute('open');
     return;
   } else if (found.length === 0) {
-    // @ts-ignore
     area.innerHTML = '';
-    // @ts-ignore
     html.list.message.classList = 'list__message_show';
   } else {
-    // @ts-ignore
-    html.list.message.classList = 'list__message';
-    // @ts-ignore
+    html.search.dialog.removeAttribute('open');
     area.innerHTML = '';
-
-    for (let i = 0; i < found.length; i++) {
-      const book = found[i];
+    found.forEach((book) => {
       const { image, title, author: authorId, id } = book;
 
       const element = document.createElement('button');
-      // @ts-ignore
       element.classList = 'preview';
       element.setAttribute('id', id);
-      element.innerHTML = /* html */ `
+      element.innerHTML = `
         <img class="preview__image" src="${image}"/>
         <div class="preview__info" data-box>
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[authorId]}</div>
-        </div> `;
-      fragment.appendChild(element);
-    }
+          <h3 class="preview__title">${title}</h3>
+          <div class="preview__author">${authors[authorId]}</div>
+        </div>`;
 
-    // @ts-ignore
+      fragment.appendChild(element);
+    });
+
     area.appendChild(fragment);
   }
-
-  // @ts-ignore
-  html.search.dialog.removeAttribute('open');
 };
-
-
