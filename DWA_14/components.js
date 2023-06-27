a3380db905aff27ca7dba0f809a4b13e22147ff6
Zamavundla <zsdouzee8@gmail.com>
2023-06-27 13:16:51 +0200

    final Commit

--- DWA_14/components.js
+++ DWA_14/components.js
@@ -1,7 +1,5 @@
 // components.js
 
-// JavaScript code for defining the custom web components
-
 // Import necessary modules from Lit
 import { html, css, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 
@@ -33,12 +31,15 @@ class PlusButton extends LitElement {
   static styles = css`
     /* Styles for the PlusButton component */
     button {
-      background-color: #64b5f6;
+      background-color: #264573;
       color: white;
       border: none;
-      padding: 0.5rem 1rem;
+      padding: 1rem;
       font-size: 1.2rem;
+      border-radius: 50%;
       cursor: pointer;
+      width: 80px;
+      height: 80px;
     }
   `;
 
@@ -58,12 +59,15 @@ class MinusButton extends LitElement {
   static styles = css`
     /* Styles for the MinusButton component */
     button {
-      background-color: #64b5f6;
+      background-color: #264573;
       color: white;
       border: none;
-      padding: 0.5rem 1rem;
+      padding: 1rem;
       font-size: 1.2rem;
+      border-radius: 50%;
       cursor: pointer;
+      width: 80px;
+      height: 80px;
     }
   `;
 
@@ -83,12 +87,15 @@ class ResetButton extends LitElement {
   static styles = css`
     /* Styles for the ResetButton component */
     button {
-      background-color: #e91e63;
+      background-color: #9c274b;
       color: white;
       border: none;
-      padding: 0.5rem 1rem;
+      padding: 1rem;
       font-size: 1.2rem;
+      border-radius: 50%;
       cursor: pointer;
+      width: 80px;
+      height: 80px;
     }
   `;
 
@@ -103,12 +110,74 @@ class ResetButton extends LitElement {
 }
 customElements.define('reset-button', ResetButton);
 
+// Define the CounterScreen component
+class CounterScreen extends LitElement {
+  static styles = css`
+    /* Styles for the CounterScreen component */
+    input {
+      font-size: 2rem;
+      text-align: center;
+      width: 100%;
+      padding: 1rem;
+      border: none;
+      background-color: #f6f6f6;
+    }
+  `;
+
+  render() {
+    return html`
+      <!-- HTML template for the CounterScreen component -->
+      <input readonly value="0" data-key="number" />
+    `;
+  }
+}
+customElements.define('counter-screen', CounterScreen);
+
+// Define the TallyCounter component
+class TallyCounter extends LitElement {
+  static styles = css`
+    /* Styles for the TallyCounter component */
+    main {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 2rem;
+    }
+
+    .counter-container {
+      margin-bottom: 2rem;
+    }
+
+    .button-container {
+      display: flex;
+      justify-content: center;
+    }
+  `;
+
+  render() {
+    return html`
+      <!-- HTML template for the TallyCounter component -->
+      <main>
+        <div class="counter-container">
+          <counter-screen></counter-screen>
+        </div>
+        <div class="button-container">
+          <plus-button data-key="add"></plus-button>
+          <minus-button data-key="subtract"></minus-button>
+        </div>
+        <reset-button data-key="reset"></reset-button>
+      </main>
+    `;
+  }
+}
+customElements.define('tally-counter', TallyCounter);
+
 // Define the FooterElement component
 class FooterElement extends LitElement {
   static styles = css`
     /* Styles for the FooterElement component */
     footer {
-      background-color: #4285f4;
+      background-color: #9c274b;
       color: white;
       padding: 1rem;
       text-align: center;
@@ -119,7 +188,7 @@ class FooterElement extends LitElement {
     return html`
       <!-- HTML template for the FooterElement component -->
       <footer>
-        <p>© 2023 Tally Counter. All rights reserved.</p>
+        <p>&copy; 2023 Tally Counter. All rights reserved.</p>
       </footer>
     `;
   }
