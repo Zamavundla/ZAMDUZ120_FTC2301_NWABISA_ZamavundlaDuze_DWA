f963b8940ee48390bff3f1439213df5ac3500996
Zamavundla <zsdouzee8@gmail.com>
2023-07-06 11:29:38 +0200

    updates

--- DWA_15/Scripts/App.js
+++ DWA_15/Scripts/App.js
@@ -1,14 +1,16 @@
 import React from "react"
+import ReactDom from "react-dom"
 import Main from "./components/Main"
-import Nav from "./components/Nav"
+import Navbar from "./components/Nav"
 
 
 export default function App(){
     return (
-        <>
-        <Main />
-        <Nav />
-        </>
+        <div className="container">
+         <Main />
+         <Navbar />
+        </div>
     )
 }
 
+ReactDom.render(<App />, document.getElementById("root"))
