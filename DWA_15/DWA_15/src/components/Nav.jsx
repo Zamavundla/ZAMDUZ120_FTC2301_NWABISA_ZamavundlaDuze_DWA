8fbdff124b1a618328c054a77085cdc4d6065bc8
Zamavundla <zsdouzee8@gmail.com>
2023-07-10 19:19:34 +0200

    Nav updates

--- DWA_15/DWA_15/src/components/Nav.jsx
+++ DWA_15/DWA_15/src/components/Nav.jsx
@@ -4,7 +4,9 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../image/th.jpg" />
+            <img src="C:\Users\suppo\OneDrive\Documents\CodeSpace Academy Challenges\Dynamic Web Applications\ZAMDUZ120_FTC2301_NWABISA_ZamavundlaDuze_DWA\DWA_15\DWA_15\src\assets\th.jpg" className="nav--icon" />
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
