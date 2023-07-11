9e86996dbb09a523b3ac1332fe27a08ea1d8eab3
Zamavundla <zsdouzee8@gmail.com>
2023-07-11 10:41:18 +0200

    Components Updates

---
+++ DWA_16/src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../assets/download.png" className="nav--logo"/>
+        </nav>
+    )
+}
